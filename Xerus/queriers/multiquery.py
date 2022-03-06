01def2e6c990029d5cea29745b64503794fdeb64
Matthew Evans <git@ml-evs.science>
2022-03-06 18:52:40 +0000

    Add missing import from bad merge

--- Xerus/queriers/multiquery.py
+++ Xerus/queriers/multiquery.py
@@ -19,6 +19,7 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 # WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 import os
+import json
 import sys
 import shutil
 from pathlib import Path
