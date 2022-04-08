802be056c7fce39e3670666ad6ad36d3b00e66fd
pedrobcst <pedrobcst@gmail.com>
2022-04-08 19:27:01 +0900

    fixed git issues?

--- Xerus/engine/gsas2riet.py
+++ Xerus/engine/gsas2riet.py
@@ -280,15 +280,14 @@ def simulate_spectra(cifpath: str, tmin: float, tmax: float, step: float, outfol
     return simul_out, refl_out, True
 
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-@blockPrinting
-=======
 
->>>>>>> Changed testing to cif reading only.
-=======
+
+
+
+
+
+
 @blockPrinting
->>>>>>> readd blockprinting decorator
 def run_gsas(powder_data: str, cif_name: str, phasename: str,
              limits: Tuple[float, float] = (10., 70.) ,max_cyc: int = 8,
              instr_params: str = INSTR_PARAMS) -> Tuple[float, str]:
